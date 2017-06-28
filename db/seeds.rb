5f52dde361b6de09d84779e0d5319854e1cfb27b
David M <davidmh.mail@gmail.com>
2017-06-27 20:08:14 -0600

    Seed development database with data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'random_data'
+
+5.times do
+	u = User.create!(
+		name: RandomData.random_name,
+		email: RandomData.random_email,
+		password: RandomData.random_sentence
+	)
+	u.confirm
+end
+
+test_user = User.create!(
+	name: "test User",
+	email: "test@example.com",
+	password: "helloworld"
+)
+test_user.confirm
+
+# admin = User.create!(
+# 	name: "Admin User",
+# 	email: "admin@example.com",
+# 	password: "helloworld",
+# 	role: "admin"
+# )
+
+users = User.all
+
+10.times do
+	Topic.create!(
+		title: RandomData.random_sentence,
+		user: users.sample
+	)
+end
+topics = Topic.all
+
+50.times do
+	bookmark = Bookmark.create!(
+		topic: topics.sample,
+		name: RandomData.random_name,
+		url: RandomData.random_url,
+		description: RandomData.random_sentence
+	)
+	bookmark.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+end
+bookmarks = Bookmark.all
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{test_user.name} #{test_user.email} created"
+puts "#{Topic.count} topics created"
+puts "#{Bookmark.count} bookmarks created"
