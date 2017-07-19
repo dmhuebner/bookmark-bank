b1ed0a43f2eb695e6448a42f2698ed7fb0f5e664
David M <davidmh.mail@gmail.com>
2017-07-19 07:28:57 -0600

    Adust incoming controller - working

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -11,6 +11,21 @@ class IncomingController < ApplicationController
 		# You put the message-splitting and business
 		# magic here.
 
+		user = User.find_by_email(params[:sender])
+		subject = params[:subject]
+		content = params[:'body-plain']
+		url = content.scan(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/)[0][0]
+
+		topics = []
+
+		subject.scan(/\B#([^,\#]+)/).each do |topic|
+			topics.push(Topic.find_or_create_by(name: topic[0].to_s.downcase))
+		end
+
+		bookmark = user.bookmarks.create(url: url)
+
+		
+
 		# Assuming all went well.
 		head 200
 	end
