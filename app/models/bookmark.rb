e6548f17c3ddeeb0e77dfda9d3658573be5071ce
David M <davidmh.mail@gmail.com>
2017-07-19 19:13:32 -0600

    Bookmark model - remove name presence validation

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -5,7 +5,8 @@ class Bookmark < ActiveRecord::Base
 	# Validations
 	validates :url, length: {minimum: 3}, presence: true
 	validates :description, length: {maximum: 100}
-	validates :name, length: {minimum: 1}, presence: true
+	validates :name, length: {minimum: 1}
+	#, presence: true
 	validates :topic, presence: true
 
 	# Scope
