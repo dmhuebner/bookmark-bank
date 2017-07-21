521de275735ac3c9244fcc191e5f08f76728a266
David M <davidmh.mail@gmail.com>
2017-07-21 14:30:50 -0600

    Adjust incoming controller - working

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,7 +1,8 @@
 class IncomingController < ApplicationController
 
 	# http://stackoverflow.com/questions/1177863/how-do-i-ignore-the-authenticity-token-for-specific-actions-in-rails
-	skip_before_action :verify_authenticity_token, only: [:create]
+	skip_before_action :verify_authenticity_token
+	skip_before_action :authenticate_user!
 
 	def create
 		# Take a look at these in your server logs
